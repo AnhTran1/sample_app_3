9291f92a9b5a2cc17bf8b6950942baa5c66b7134
Tran Anh <anhtran@anhtran-K45VD.(none)>
2014-06-23 12:33:53 +0700

    All user chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
+  end
+  def index
+    @users = User.all
   end
   def show
     @user = User.find(params[:id])
@@ -16,11 +21,11 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -33,5 +38,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
