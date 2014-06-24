da1c8986e0f43b659bf13a4ed9570d175362aa2f
Tran Anh <anhtran@anhtran-K45VD.(none)>
2014-06-24 15:32:03 +0700

    finish chapter 11Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   def new
     @user = User.new
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted."
     redirect_to users_url
   end
+  
   def index
     @users = User.paginate(page: params[:page])
   end
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -27,6 +45,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def edit
     #@user = User.find(params[:id])
   end
@@ -40,6 +59,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
   private
 
     def user_params
@@ -54,10 +74,12 @@ class UsersController < ApplicationController
         redirect_to signin_url, notice: "Please sign in."
       end
     end
+    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+    
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
